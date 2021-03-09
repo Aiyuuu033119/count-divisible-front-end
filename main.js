cc895c059d2d699368de7a9615ff5ba8036eb872
unknown <idestura35@gmail.com>
2021-03-09 12:55:53 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+    $('.getResult').on('click', function (e) {
+        e.preventDefault();
+
+        var x = $('.x').val();
+        var y = $('.y').val();
+        var p = $('.p').val();
+
+        var data = new FormData();
+
+        var hasError;
+
+        if (x == '' && y == '' && p == '') {
+            $('.x').attr('style', 'border:1px solid #ff3737');
+            $('.y').attr('style', 'border:1px solid #ff3737');
+            $('.p').attr('style', 'border:1px solid #ff3737');
+            $('.x').val('');
+            $('.y').val('');
+            $('.p').val('');
+            $('.x').attr('placeholder','Empty Input');
+            $('.y').attr('placeholder','Empty Input');
+            $('.p').attr('placeholder','Empty Input');
+            hasError = true;
+        } else {
+
+            if ($.isNumeric(x)==false) {
+                $('.x').attr('style', 'border:1px solid #ff3737');
+                $('.x').val('');
+                $('.x').attr('placeholder','Invalid Input');
+                hasError = true;
+            }
+            if ($.isNumeric(y)==false) {
+                $('.y').attr('style', 'border:1px solid #ff3737');
+                $('.y').val('');
+                $('.y').attr('placeholder','Invalid Input');
+                hasError = true;
+            }
+            if ($.isNumeric(p)==false) {
+                $('.p').attr('style', 'border:1px solid #ff3737');
+                $('.p').val('');
+                $('.p').attr('placeholder','Invalid Input');
+                hasError = true;
+            }else{
+                $('.x').attr('style', 'border:1px solid rgb(161, 161, 161)');
+                $('.y').attr('style', 'border:1px solid rgb(161, 161, 161)');
+                $('.p').attr('style', 'border:1px solid rgb(161, 161, 161)');
+                hasError = false;
+            }
+            
+            
+        }
+
+        if (x == '') {
+            $('.x').attr('style', 'border:1px solid #ff3737');
+            $('.x').val('');
+            $('.x').attr('placeholder','Empty Input');
+            hasError = true;
+        }
+        if (y == '') {
+            $('.y').attr('style', 'border:1px solid #ff3737');
+            $('.y').val('');
+            $('.y').attr('placeholder','Empty Input');
+            hasError = true;
+        }
+        if (p == '') {
+            $('.p').attr('style', 'border:1px solid #ff3737');
+            $('.p').val('');
+            $('.p').attr('placeholder','Empty Input');
+            hasError = true;
+        }
+
+        if (hasError == false) {
+            data.append('x', x);
+            data.append('y', y);
+            data.append('p', p);
+
+            $.ajax({
+                url: "https://count-divisible.herokuapp.com/",
+                data: data,
+                type: "POST",
+                contentType: false,
+                processData: false,
+                success: function (data) {
+
+                    var json = JSON.parse(data);
+                    
+                    $('.result').text(json);
+                }
+            });
+        }
+    });
+
+});
